e322dd8489310c3ca4963c5e97dcfb51f276a560
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-08-03 00:31:35 +0900

    Feat: Security의 모든 요청을 허가한다.

--- pdf_back/src/main/java/org/capsc/pdf/config/SecurityConfig.java
+++ pdf_back/src/main/java/org/capsc/pdf/config/SecurityConfig.java
@@ -1,21 +1,29 @@
 package org.capsc.pdf.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    public static WebConfig webConfig;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // 모든 요청 허용
-                );
+                                .anyRequest().permitAll()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         return http.build();
     }
 }
