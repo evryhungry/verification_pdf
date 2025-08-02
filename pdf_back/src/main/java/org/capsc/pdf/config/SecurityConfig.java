39866b2921e862bcadc37c194eaf6b03af229a0d
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-08-03 00:01:01 +0900

    Feat: add config 추가

---
+++ pdf_back/src/main/java/org/capsc/pdf/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package org.capsc.pdf.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // 모든 요청 허용
+                );
+        return http.build();
+    }
+}
